118f3c3aac6596595091dfeff69dbbcabf26ed13
VictorM-Coder <martinsvictor6781@gmail.com>
2024-12-26 20:12:09 -0300

    feat: enable and disable menu buttons

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,4 +1,17 @@
 document.getElementById("btn-cancel")
-    .addEventListener("click", function(){
+    .addEventListener("click", function () {
         window.close()
     })
+
+btnAddlink = document.getElementById("btn-addlink")
+btnLibrary = document.getElementById("btn-library")
+
+btnAddlink.addEventListener("click", () => {
+    btnAddlink.disabled = true
+    btnLibrary.disabled = false
+})
+
+btnLibrary.addEventListener("click", () => {
+    btnLibrary.disabled = true
+    btnAddlink.disabled = false
+})
